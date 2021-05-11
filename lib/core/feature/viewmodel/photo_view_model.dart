002b9579fb1315fb728be3f794794c0fd9e6e852
Elgiz Cebrayilov <elgizcebrayilov1992@gmail.com>
2021-05-11 18:46:45 +0400

    test

---
+++ lib/core/feature/viewmodel/photo_view_model.dart
@@ -0,0 +1,8 @@
+import 'package:mobx/mobx.dart';
+part 'photo_view_model.g.dart';
+
+class PhotosViewModel = _PhotosViewModelBase with _$PhotosViewModel;
+
+abstract class _PhotosViewModelBase with Store {
+  
+}
